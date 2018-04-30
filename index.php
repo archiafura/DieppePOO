d49645f687ed8e57e852a5ca0dc98655566f86f6
archiafura <archiafura@icloud.com>
2018-04-30 18:19:51 +0200

    essai poo formulaire inscription

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 define("PATHCONF", "./conf/");
 date_default_timezone_set('Europe/Paris');
 
+require_once "./classes/inscription.php";
+
+
 require_once "./function/classAutoLoader.php";
 spl_autoload_register('classAutoLoader');
 
@@ -10,6 +13,35 @@ require_once "./classes/Log.php";
 spl_autoload_register('Log');
 
 
+
+$inscr = new inscription();
+
+echo $inscr->input('nom');
+echo $inscr->input('pseudo');
+echo $inscr->submit();
+
+
+
+/*
+define('INI_FILE', 'inscription.ini');
+
+$ini = parse_ini_file(INI_FILE);
+$pseudo = isset($ini['Pseudo']) ? trim($ini['Pseudo']) : '';
+$id = isset($ini['Id']) ? trim($ini['Id']) : '';
+$option = isset($ini['Option']) ? trim($ini['Option']) : '';
+?>
+
+<form method="post" action="updates.php">
+    Id : <input type="text" name="id" value="<?php echo $id; ?>" />
+    <br />
+    Pseudo : <input type="text" name="id" value="<?php echo $pseudo; ?>" />
+    <br/>
+    Option : <input type="text" name="id" value="<?php echo $option; ?>" />
+    <br />
+    <input type="submit" name="submit" value="Valider" />
+</form>
+
+*/
 
 
 $test = new Form(PATHCONF, "inscription");
